ae8f9786bd32caefcc74ab0951fb43c23b9261c7
maryd-k <maryia.dyliuk@gmail.com>
2021-03-03 10:34:27 +0300

    feat: implement function createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let teamName='';
+  let k;
+  if (Array.isArray(members) != true || members.length == 0 )
+  return false;
+  else
+  {
+    for (let i=0; i<members.length; i++)
+    {
+     if ((typeof members[i]) === 'string' )
+     {
+        k=0;
+        while(members[i].charAt(k)==' ')
+        {
+          k++;
+        }
+        teamName = teamName + members[i].charAt(k).toUpperCase();
+      }
+    }
+  }
+  let newTeamName= teamName.split('').sort().join('');
+  return newTeamName;
 };
